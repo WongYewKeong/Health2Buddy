01373c58f0cc71b5e514ce7089e940ba52f05373
Amir Hakim Mior <a178453@siswa.ukm.edu.my>
2021-01-07 18:48:12 +0800

    amir hakim mior workout module

--- app/src/main/java/com/example/healthapp/WorkoutFragment.java
+++ app/src/main/java/com/example/healthapp/WorkoutFragment.java
@@ -1,9 +1,11 @@
 package com.example.healthapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -14,16 +16,46 @@ import androidx.lifecycle.ViewModelProvider;
 
 import com.example.healthapp.R;
 
-public class WorkoutFragment extends Fragment {
+public class WorkoutFragment extends Fragment implements View.OnClickListener{
 
 
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
-        return inflater.inflate(R.layout.fragment_workout, container, false);
 
 
+        View view =  inflater.inflate(R.layout.fragment_workout, container, false);
+        ImageButton overallbutton = view.findViewById(R.id.overallbutton);
+        ImageButton focusedbutton = view.findViewById(R.id.focusedbutton);
+        ImageButton advancedbutton = view.findViewById(R.id.advancedbutton);
+        overallbutton.setOnClickListener(this);
+        focusedbutton.setOnClickListener(this);
+        advancedbutton.setOnClickListener(this);
+        return view;
+
+
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.overallbutton:
+                Intent intent = new Intent ( getActivity(),overallworkout.class);
+                startActivity(intent);
+                break;
+
+            case R.id.focusedbutton:
+                Intent intent1 = new Intent ( getActivity(),focusedworkout.class);
+                startActivity(intent1);
+                break;
+
+            case R.id.advancedbutton:
+                Intent intent2 = new Intent ( getActivity(),advanceworkout.class);
+                startActivity(intent2);
+                break;
+        }
 
     }
 }
