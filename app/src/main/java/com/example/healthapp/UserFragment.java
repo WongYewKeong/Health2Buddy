74976eb0eb7b5bc7609b4a87103c063851d77579
Intan Maisarah <a174269@siswa.ukm.edu.my>
2021-01-10 04:08:10 +0800

    user fragment gender problem

--- app/src/main/java/com/example/healthapp/UserFragment.java
+++ app/src/main/java/com/example/healthapp/UserFragment.java
@@ -1,6 +1,9 @@
 package com.example.healthapp;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,6 +15,7 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 
 import com.facebook.login.LoginManager;
@@ -30,10 +34,20 @@ public class UserFragment extends Fragment {
     GoogleSignInOptions gso;
     GoogleSignInClient signInClient;
 
-    TextView tvName ;
-    EditText etAge , etGender ;
+    ConstraintLayout cl_userage, cl_usergender;
+
+    TextView tvName , tvGender;
+    EditText etAge ;
     Button btnUpdate;
     ImageView profile;
+
+    SharedPreferences sharedPref;
+    SharedPreferences.Editor editor;
+
+    String SP_GENDER = "gender";
+
+    String[] gender = {"Male","Female"};
+    AlertDialog alertDialog;
 
 
 
@@ -46,10 +60,13 @@ public class UserFragment extends Fragment {
 
         Button signout = root.findViewById(R.id.btn_signout);
         tvName = root.findViewById(R.id.tv_name);
+        tvGender = root.findViewById(R.id.tv_gender);
         profile=root.findViewById(R.id.iv_profilepicture);
+        cl_userage = root.findViewById(R.id.cl_user_age);
+        cl_usergender = root.findViewById(R.id.cl_user_gender);
 
         etAge = root.findViewById(R.id.et_age);
-        etGender = root.findViewById(R.id.et_gender);
+
 
         btnUpdate =root.findViewById(R.id.bt_update);
         firebaseAuth = FirebaseAuth.getInstance();
@@ -95,6 +112,35 @@ public class UserFragment extends Fragment {
 
         tvName.setText(value.getString("Username"));
     }
+
+
+    private void ShowGenderOptions() {
+
+        androidx.appcompat.app.AlertDialog.Builder builder = new androidx.appcompat.app.AlertDialog.Builder(UserFragment.this);
+        builder.setTitle("Select your gender");
+        builder.setSingleChoiceItems(gender, sharedPref.getInt(SP_GENDER, 0), new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int i) {
+
+                switch(i){
+                    case 0:
+                        editor.putInt(SP_GENDER,0);
+                        editor.commit();
+                        break;
+
+                    case 1:
+                        editor.putInt(SP_GENDER,1);
+                        editor.commit();
+                        break;
+
+                }
+                alertDialog.dismiss();
+                tvGender.setText(gender[sharedPref.getInt(SP_GENDER,0)]);
+            }
+        });
+
+    }
+
 
 
 
